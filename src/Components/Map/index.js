7f7562c22ebacff7319fea2f606347e45792df59
Juan <juanxcerv@berkeley.edu>
2018-11-03 01:26:38 -0700

    Fixed markers and set bounds

--- src/Components/Map/index.js
+++ src/Components/Map/index.js
@@ -9,28 +9,37 @@ export class MapView extends React.Component {
     this.state = {
       markers: [],
     }
+    this.fetchPlaces = this.fetchPlaces.bind(this);
   }
   fetchPlaces(mapProps, map) {
     const {locations} = mapProps;
+    console.log(locations);
     const markers = [];
     for(var i = 0; i < locations.length; i++) {
       var marker = <Marker
         title={'The marker`s title will appear as a tooltip.'}
         name={'SOMA'}
         position={locations[i]} 
+        key={i}
       />
       markers.push(marker);
     }
     this.setState({markers})
   }
+  
   render() {
+    var bounds = new this.props.google.maps.LatLngBounds();
+    for (var i = 0; i < this.props.locations.length; i++) {
+      bounds.extend(this.props.locations[i]);
+    }
     return (
       <Map 
         google={this.props.google} 
         initialCenter = {this.props.center} 
         zoom={14} 
         onReady={this.fetchPlaces}
-        bounds={this.props.locations}
+        locations={this.props.locations}
+        bounds={bounds}
       >
         {this.state.markers} 
       </Map>
