cf37346a557b7e109ac449498f2cffd64a65defc
Juan <juanxcerv@berkeley.edu>
2018-11-03 23:37:43 -0700

    added needs to personmodal, needs styling

--- src/Components/PersonModal/index.js
+++ src/Components/PersonModal/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Modal } from 'semantic-ui-react';
+import { Container, Header, Modal } from 'semantic-ui-react';
 
 class PersonModal extends Component {
   render() {
@@ -8,10 +8,20 @@ class PersonModal extends Component {
         open={this.props.person!=null}
         onClose={this.props.closePersonInfo}
       >
-        <Modal.Header>{this.props.person && this.props.person.name}</Modal.Header>
-        <Modal.Content >
+      <Container text>
+        <Header as='h2'>Name:</Header>
+        <p>
+          {this.props.person && this.props.person.name}
+        </p>
+        <Header as='h2'>Bio:</Header>
+        <p>
           {this.props.person && this.props.person.bio}
-        </Modal.Content>
+        </p>
+        <Header as='h2'>Needs:</Header>
+        <p>
+          {this.props.person && this.props.person.needs}
+        </p>
+      </Container>
       </Modal>
     );
   }
